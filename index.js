03c33f8434a88cff3690f4dc19e44b576572f055
Tarun <75876644+tarunbommali@users.noreply.github.com>
2024-06-02 12:33:40 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const serverless = require("serverless-http");
-const productRoute = require("../routes/product.route.js");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+const productRoute = require('./routes/product.route.js');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
 // Middleware
 app.use(cors({
-  origin: ["https://ekart-client.vercel.app"],
-  methods: ["POST", "GET", "PUT", "DELETE"],
+  origin: ['https://ekart-client.vercel.app'],
+  methods: ['POST', 'GET', 'PUT', 'DELETE'],
   credentials: true
 }));
 
@@ -17,20 +17,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use("/.netlify/functions/api", productRoute);
-
-const PORT = process.env.PORT || 5000;
+app.use('/', productRoute);
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Connected to database!");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+    console.log('Connected to database!');
   })
   .catch((err) => {
-    console.error("Connection failed:", err.message);
+    console.error('Connection failed:', err.message);
   });
 
 module.exports.handler = serverless(app);
